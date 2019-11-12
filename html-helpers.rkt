504c041bc36a765d76873a9ffe14230f9170a569
Lindsey Handley <lindseyhandley@gmail.com>
2019-11-11 20:58:36 -0800

    freaking awesome transparent to solid nav bar

---
+++ html-helpers.rkt
@@ -0,0 +1,96 @@
+#lang at-exp racket
+
+(provide normal-content
+         homepage-content)
+
+(require website/bootstrap
+         "./css.rkt"
+         "./img.rkt")
+
+(define (normal-content . more)
+  (content
+    (normal-navbar)
+    (container 
+      id: "main"
+      more)
+    (normal-footer)))
+
+(define (homepage-content . more)
+  (content 
+    (normal-navbar)
+    (div id: "main"
+         more)
+    (normal-footer)))
+
+(define (normal-navbar)
+
+  (list
+   (style/inline
+     @~a{
+      .navbar.solid {
+          background-color: #343a40 !important; 
+          transition: background-color 1s ease 0s;
+        }
+     }) 
+   (script/inline
+     @~a{
+        $(document).ready(function() {
+                // Transition effect for navbar 
+                $(window).scroll(function() {
+                  // checks if window is scrolled more than 500px, adds/removes solid class
+                  if($(this).scrollTop() > 475) { 
+                      $('.navbar').addClass('solid');
+                  } else {
+                      $('.navbar').removeClass('solid');
+                  }
+                });
+        });
+     })
+  (nav class: "navbar fixed-top navbar-expand-md bg-transparent" 
+    (a class: "navbar-brand" 
+       href: "/index.html"
+       style: (properties color: "white") 
+       "Lindsey Handley, Ph.D.") 
+    (ul class: "navbar-nav ml-auto"
+      (my-nav-link 
+        "/index.html" "Homepage")))))
+
+(define (my-nav-link to text)
+  (nav-item
+    (a class: "nav-link mr-3 text-white"
+       href: (pathify (add-path-prefix to))
+       text)))
+
+(define (normal-footer)
+  (footer class: "pt-4"
+          style: (properties background-color: "#343a40")
+    (row style: (properties margin: 15 
+                            color: "white")
+      (col-6
+        (row
+          (col-3
+            (img  class: "img-fluid rounded-circle"
+                  src: "https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/p960x960/27503701_10100408522873930_2950324374038593153_o.jpg?_nc_cat=100&_nc_oc=AQly12clcDf1W7jSyVelBFu587Trr6mwLbN82RsTGSjp_itfZX-8znn7H-XVh-umEas&_nc_ht=scontent-lax3-1.xx&oh=404ef0b0573d2363d37f115cb6ba68ef&oe=5E42131F"))
+          (col-9
+            (h3 "WHO AM I?")
+            (p  
+               "Summary of who I am..."))))
+      (col-3
+        (h3 "LINKS")
+          (ul class: "list-unstyled"
+            (li 
+              (p "My Projects")
+            (li 
+              (p "Blog")))))
+      (col-3
+        (h3 "CONNECT")
+          (ul class: "list-unstyled"
+            (li 
+              (p "lindseyhandley [at] gmail [dot] com"))
+            (li
+              (p "INSERT SOCIAL MEDIA ICONS"))
+            (li 
+              (p "Copyright 2020"))
+            (li
+              (p "All rights reserved")))))))
+
--- index.rkt
+++ index.rkt
@@ -2,6 +2,7 @@
  
 (require website/bootstrap
           racket/runtime-path
+          "./html-helpers.rkt"
           "./css.rkt"
           "./img.rkt")
 
@@ -11,7 +12,7 @@
                       margin: 0
                       background-image: "url(https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/52818271_10100521841507480_8053906936699027456_o.jpg?_nc_cat=101&_nc_eui2=AeFC_kBeAKLsazK1nnwD58qrLgay9OeK0ZKb_Fzkeuf9vRSlYSu7WWFoZdQylqxBp8t3ysDYPyucr9nd7SoG8RAxRryOqW4Gi7vegfxi0rv02A&_nc_oc=AQmKkd6hu9x5d5udaF9x_qGhC4HTMYK6Y5yts8JoEVBJohwXTH9H9ACj02JvwY1JKjo&_nc_ht=scontent-lax3-2.xx&oh=1bdc32f4c0a3fb389e5a86dc0ce5d691&oe=5E610C66)"
                       background-size: "cover"
-                      height: "100%")
+                      height: "80%")
              class: "d-flex align-items-center"
                     (container
                       (div style: (properties
@@ -92,46 +93,16 @@
             "Someone famous")))
       )))
 
-(define (my-footer)
-  (footer class: "pt-4"
-    (row style: (properties margin: 15)
-      (col-6
-        (row
-          (col-3
-            (img  class: "img-fluid rounded-circle"
-                  src: "https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/p960x960/27503701_10100408522873930_2950324374038593153_o.jpg?_nc_cat=100&_nc_oc=AQly12clcDf1W7jSyVelBFu587Trr6mwLbN82RsTGSjp_itfZX-8znn7H-XVh-umEas&_nc_ht=scontent-lax3-1.xx&oh=404ef0b0573d2363d37f115cb6ba68ef&oe=5E42131F"))
-          (col-9
-            (h3 "WHO AM I?")
-            (p  
-               "Summary of who I am..."))))
-      (col-3
-        (h3 "LINKS")
-          (ul class: "list-unstyled"
-            (li 
-              (p "My Projects")
-            (li 
-              (p "Blog")))))
-      (col-3
-        (h3 "CONNECT")
-          (ul class: "list-unstyled"
-            (li 
-              (p "lindseyhandley [at] gmail [dot] com"))
-            (li
-              (p "INSERT SOCIAL MEDIA LINKS"))
-            (li 
-              (p "\u00a9 2020 copyright: lindsey handley")))))))
- 
-
 (define my-site
   (list
     (bootstrap-files) 
     (my-css)
     (imgs)
     (page index.html 
-      (content
+      (homepage-content
+        ;Debt:
         (header-jumbotron)
         (my-projects-section)
-        (my-blog)
-        (my-footer)))))
+        (my-blog)))))
 
 (render my-site #:to "output")
